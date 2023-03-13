408abf996d44ddd9c347659686345a9fca6638ec
chenhebin <>
2023-03-13 10:22:14 +0800

    home

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,43 @@
-<template>
-  <div>
-    欢迎来到我的世界
+<template >
+  <div style="height: 100%;width: 100%">
+    <div style="height: 100%;width: 100%" class="home">
+      <ul  v-infinite-scroll="load" class="infinite-list" style="overflow: auto">
+        <li v-for="i in count" :key="i" class="infinite-list-item">{{ i }}</li>
+      </ul>
+    </div>
   </div>
+
 </template>
 
-<script>
-export default {
-  name: "home"
+<script lang="ts" setup>
+import { ref } from 'vue'
+const count = ref(0)
+const load = () => {
+  count.value += 2
 }
 </script>
 
-<style scoped>
-
+<style>
+.home{
+  display: flex;
+}
+.infinite-list {
+  flex: 1;
+  height: 1000px;
+  padding: 0;
+  margin-left: 0;
+  list-style: none;
+}
+.infinite-list .infinite-list-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  background: var(--el-color-primary-light-9);
+  margin: 10px;
+  color: var(--el-color-primary);
+}
+.infinite-list .infinite-list-item + .list-item {
+  margin-top: 10px;
+}
 </style>
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,19 +1,35 @@
-<script setup>
+<script>
 import { StarrySky } from '../../../packages/index';
 import { UserFilled, Lock, Promotion, Collection } from '@element-plus/icons-vue';
 import { ref } from 'vue'
+import { useRouter } from "vue-router";
 
 
-const input1 = ref('')
-const input2 = ref('');
+export default {
 
-// export default {
-//   methods: {
-//     to(){
-//       this.$router.push({ path: '/home' });
-//     }
-//   }
-// }
+    name: "login",
+
+    setup() {
+        const input1 = ref('');
+        const input2 = ref('');
+        const input3 = ref('');
+        const dialogVisible = ref(false);
+        const router = useRouter();
+        const tohome = () => {
+            router.push({ path: '/home' });
+        }
+        return {
+            tohome,
+            input1,
+            input2,
+            input3,
+            dialogVisible
+        }
+
+    }
+}
+
+
 </script>
 
 <template>
@@ -21,22 +37,44 @@ const input2 = ref('');
     <div id="main">
         <div id="login-box">
             <div>
-                <span style="margin-left: 20px; font-size: x-large">Hi</span>
+                <span style="margin-left: 20px; font-size: xx-large; font-family: 'STCaiyun'">LUATS</span><br>
+                <span style="margin-left: 20px; font-size: x-small; letter-spacing: 5px; font-family: 'STXingkai'">世界因你而精彩</span>
             </div>
             <div>
                 <el-input v-model="input1" class="w-50 m-2" placeholder="Please input name" :prefix-icon="UserFilled" />
             </div>
             <div>
-                <el-input v-model="input2" type="password" placeholder="Please input password" show-password
-                    :prefix-icon="Lock" />
+                <el-input v-model="input2" class="w-50 m-2" type="password" placeholder="Please input password" :prefix-icon="Lock" />
             </div>
             <div>
-                <el-button :key="success" :type="success" text style="margin-left: 20px" :icon="Promotion" @click="to">登录</el-button>
-                <el-button :key="primary" :type="primary" text style="float: right;" :icon="Collection">注册</el-button>
+                <el-button :key="success" :type="success" text style="margin-left: 20px" :icon="Promotion"
+                    @click="tohome">登录</el-button>
+                <el-button :key="primary" :type="primary" text style="float: right;" :icon="Collection"
+                    @click="dialogVisible = true">注册</el-button>
             </div>
+
         </div>
 
     </div>
+    <!-- 注册弹窗 -->
+    <el-dialog v-model="dialogVisible" title="LUATS 注册" width="50%" draggable align-center>
+        <div>
+            用户名:<el-input v-model="input1" placeholder="用户名小于6位" :prefix-icon="UserFilled" />
+        </div>
+        <div style="margin-top: 10px;">
+            密码:<el-input v-model="input2" type="password" placeholder="密码小于12位" :prefix-icon="Lock"  />
+        </div>
+        <div style="margin-top: 10px;">
+            确认密码:<el-input v-model="input3" type="password" placeholder="请再次确认密码" :prefix-icon="Lock"  />
+        </div>
+        <template #footer>
+            <span class="dialog-footer">
+                <el-button type="primary" @click="dialogVisible = false">
+                    Go
+                </el-button>
+            </span>
+        </template>
+    </el-dialog>
 </template>
 
 <style>
